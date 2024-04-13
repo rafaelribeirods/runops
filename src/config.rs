38dc57c2d0a0c8d5fe275d21ab333f6e1e590e7d
Rafael Ribeiro da Silva <rafaelribeiros1997@gmail.com>
2024-04-13 11:50:31 -0300

    First version

---
+++ src/config.rs
@@ -0,0 +1,24 @@
+use std::fs;
+
+use anyhow::{Result, anyhow};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct ConfigConnection {
+    pub target: String,
+    pub engine: String,
+    pub port: u16
+}
+
+pub fn load() -> Result<Vec<ConfigConnection>> {
+    let contents = get_file_contents()?;
+    let connections = serde_yaml::from_str::<Vec<ConfigConnection>>(&contents)
+        .map_err(|err| anyhow!(format!("Could not parse the config file: {}", err)))?;
+
+    Ok(connections)
+}
+
+fn get_file_contents() -> Result<String> {
+    fs::read_to_string("config.yaml")
+        .map_err(|err| anyhow!(format!("Error reading the config file: {}", err)))
+}
---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use std::{thread, time::Duration};
+
+use anyhow::Result;
+use config::ConfigConnection;
+use tokio::{io::{self, AsyncWriteExt}, process::Command};
+
+mod config;
+
+#[tokio::main]
+async fn main() {
+
+    let connections_result: Result<Vec<ConfigConnection>> = config::load();
+
+    if let Err(err) = connections_result {
+        println!("Error: {}", err);
+        return;
+    }
+
+    let connections = connections_result.unwrap();
+
+    println!("Checking Runops authentication...");
+
+    if !is_authenticated().await {
+        println!("Not authenticated");
+        println!("Try running 'runops login' on your terminal...");
+        return;
+    }
+
+    println!("Authenticated!");
+
+    for connection in connections {
+        Command::new("rproxy")
+            .arg(connection.engine)
+            .arg("-c")
+            .arg(connection.target)
+            .arg("-p")
+            .arg(connection.port.to_string())
+            .arg("&")
+            .spawn()
+            .expect("Could not start child process");
+    }
+
+    loop {
+        thread::sleep(Duration::from_secs(1));
+    }
+}
+
+async fn is_authenticated() -> bool {
+    let args = ["tasks", "list"];
+    let output = Command::new("runops")
+        .args(&args)
+        .output()
+        .await
+        .expect("Failed to execute command");
+
+    if output.status.success() {
+        let output_str = String::from_utf8_lossy(&output.stdout);
+        !output_str.contains("Invalid credentials")
+    }
+    else {
+        io::stderr().write_all(&output.stderr).await.unwrap();
+        std::process::exit(1);
+    }
+}
